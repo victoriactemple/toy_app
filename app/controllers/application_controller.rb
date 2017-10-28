539295d3f7b1004b6b7f2c96b8e2e370a8731f92
victoriactemple <victoriactemple@gmail.com>
2017-10-28 18:04:11 -0400

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello from the toy shop"
-  end
+  # def hello
+  #   render html: "hello from the toy shop"
+  # end
 end
